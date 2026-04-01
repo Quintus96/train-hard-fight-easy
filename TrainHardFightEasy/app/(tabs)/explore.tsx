3b4e0daa862b9e878e084b007670f96e66164302
Quintus <quintus.deacon@entelect.co.za>
2026-04-01 14:15:30 +0200

    finally get this pos working

---
+++ TrainHardFightEasy/app/(tabs)/explore.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl font-bold text-blue-500">
+        NativeWind is working! 🎉
+      </Text>
+    </View>
+  );
+}
